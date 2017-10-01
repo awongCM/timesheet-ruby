4143f21b5ba46856feca1963af0f8a6f25b5b6bf
Andy Chee Ming Wong <andy@creativelicence.com.au>
2017-10-01 18:10:33 +1100

    first crud app form - employees

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170911071413) do
+ActiveRecord::Schema.define(version: 20171001053753) do
+
+  create_table "employees", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "title"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
---
+++ spec/factories/employees.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :employee do
+    title "MyString"
+    first_name "MyString"
+    last_name "MyString"
+  end
+end
