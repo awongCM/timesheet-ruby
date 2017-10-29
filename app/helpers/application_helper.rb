23cf89c7c91c4f2af3c0fa8b4f4d8372223deb93
awongCM <awong.cm@gmail.com>
2017-10-29 16:07:21 +1100

    added userHasEmployeeProfile helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  #Checks current_user session has completed his employee profile
+  def userHasEmployeeProfile(current_user)
+    if !Employee.exists?(:user_id => current_user.id)
+      return false
+    else 
+      return true
+    end
+  end
 end
