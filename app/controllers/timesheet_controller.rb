b01bfbc410497d95f040ad774a799723b9a83832
awongCM <awong.cm@gmail.com>
2017-11-11 23:09:42 +1100

    added timesheet landing page; added timesheet controller methods

--- app/controllers/timesheet_controller.rb
+++ app/controllers/timesheet_controller.rb
@@ -1,7 +1,46 @@
 class TimesheetController < ApplicationController
-  #TODO
   def index
     
   end
+
+  def new
+    @timesheet = Timesheet.new
+  end
+
+  def edit
+    @timesheet = Timesheet.find(params[:id])    
+  end
+
+  def create
+    @timesheet = Timesheet.new(timesheet_params)
+    @timesheet.save
+
+    redirect_to timesheet_index_path
+  end
+
+  def update
+    @timesheet = Timesheet.find(params[:id])
+    
+    if @timesheet.update(timesheet_params)
+        redirect_to timesheet_index_path
+    else
+        render 'edit'
+    end
+
+  end
+
+  def destroy
+    @timesheet = Timesheet.find(params[:id])
+    @timesheet.destroy
+
+    redirect_to timesheet_index_path
+  end
+
+  # TODOS
+  private
+  def timesheet_params
+      params.require(:timesheet).permit(:title, :first_name, :last_name, :date_of_birth, :job_title, :address).merge(user_id: current_user.id)
+  end
+
 
 end
