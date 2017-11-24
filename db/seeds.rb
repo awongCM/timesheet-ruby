600d4097577c01b2283a2abe10169d8f1fc6ba86
awongCM <awong.cm@gmail.com>
2017-11-24 21:27:49 +1100

    added seed migration sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# TODO - simplest approach seeding data for tables with simpel relationships for now
+# next idea is to use actual csv data import rather than just plain ruby code
+
+5.times do |i|
+  user = User.create!(username: "username_0#{i}", email: "username_0#{i}@example.com", password: "username_0#{i}_pwd567" , password_confirmation: "username_0#{i}_pwd567")
+  employee = Employee.create!(title: "Mr", first_name: "User", last_name: "Name_0#{i}", user_id: user.id)
+  20.times do |j|
+    Timesheet.create(title: "Timesheet_#{j}", description:"timesheet_done_by_username_0#{i}",  total_hours: rand(2..15) , entry_date: rand(Time.now.beginning_of_week.to_date..Time.now.end_of_week.to_date), employee_id: employee.id)
+  end
+end
