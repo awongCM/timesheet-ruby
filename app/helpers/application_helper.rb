1aea4395e4c82a0d7592ff406248e652a141eec0
awongCM <awong.cm@gmail.com>
2017-12-28 12:32:39 +1100

    implemented date time display in the top right hand corner of screen

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,10 @@ module ApplicationHelper
       return true
     end
   end
+
+  def global_format_datetime(time, timezone)
+    time = time.to_time
+    content_tag(:span, I18n.l(time, format: '%A, %d-%b-%Y - %T %p'), data: {timezone: timezone, time: time.iso8601})
+  end
   
 end
