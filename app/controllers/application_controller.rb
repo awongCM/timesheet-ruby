503813d7bbf131fc30614c6b736670e8fd09e9aa
awongCM <awong.cm@gmail.com>
2017-12-29 18:37:23 +1100

    implemented static pages for 400, 500 error pages; added page authorization errors; modify permission for individual employee to update timesheet based on their logged_in session id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  # TODO - Authorization Error Handling
+  rescue_from CanCan::AccessDenied do |exception|
+    render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
+  end
+
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -33,6 +38,10 @@ class ApplicationController < ActionController::Base
       end
    
     end
+  end
+
+  def current_ability
+    @current_ability ||= Ability.new(current_user, @session_employee_id)
   end
 
 
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,7 +1,8 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
+  # TODO - need to store and pass employee session id
+  def initialize(user, session_employee_id)
     user ||= User.new
 
     if user.has_role? "admin"
@@ -11,7 +12,8 @@ class Ability
       can [:create, :read, :update], [Employee, Timesheet]
     elsif user.has_role? "user"
       can [:read, :update], Employee, :user_id => user.id
-      can [:create, :read], Timesheet, :employee_id => @session_employee_id
+      can [:create, :read], Timesheet
+      can :update, Timesheet, :employee_id => session_employee_id
       # TODO
       can [:create, :update], User, :user_id => user.id
     end
