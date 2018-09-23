a14d80c705c70fee053ae3b034a99d92c64bcd86
awongCM <awong.cm@gmail.com>
2018-09-23 22:47:04 +1000

    changed date format display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
 
   def global_format_datetime(time, timezone)
     time = time.to_time
-    content_tag(:span, I18n.l(time, format: '%A, %d-%b-%Y - %T %p'), data: {timezone: timezone, time: time.iso8601})
+    content_tag(:span, I18n.l(time, format: '%A, %d/%b/%Y - %T %p'), data: {timezone: timezone, time: time.iso8601})
   end
   
 end
