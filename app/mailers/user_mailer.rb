c8dafff76465842e3000f8c91b187d2fb74a69d1
awongCM <awong.cm@gmail.com>
2019-02-24 17:16:12 +1100

    todos for receiving emails in app..?

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,23 @@ class UserMailer < ApplicationMailer
     @url  = 'http://localhost:4000/user/login'
     mail(to: @user.email, subject: 'Welcome to your Timesheet Ruby System')
   end
+
+  # TODO - receive emails
+  def receive(email)
+    page = Page.find_by(address: email.to.first)
+    page.emails.create(
+      subject: email.subject,
+      body: email.body
+    )
+
+    if email.has_attachments?
+      email.attachments.each do |attachment| 
+        page.attachments.create({
+          file: attachment,
+          description: email.subject
+        })
+      end      
+    end
+  end
+  
 end
